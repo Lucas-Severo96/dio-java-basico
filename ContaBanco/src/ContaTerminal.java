259dcb46743b3cbbd3c883a8067e2edad5f5239e
Lucas Leite Severo <lucasleite_14.1@hotmail.com>
2024-10-05 14:58:43 -0300

    Primeira versão funcional da classe ContaTerminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,32 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da Conta Bancária!");
+        int numConta = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da Agência!");
+        String numAgencia = scanner.next();
+
+        System.out.println("Por favor, digite o seu primeiro Nome:");
+        String primeiroNome = scanner.next();
+
+        System.out.println("Por favor, digite o seu Segundo Nome:");
+        String segundoNome = scanner.next();
+
+        System.out.println("Digite o saldo da sua Conta Bancária:");
+        double saldoConta = scanner.nextDouble();
+
+        System.out.printf("Olá " + primeiroNome + " " + segundoNome + 
+        " obrigado por criar uma conta em nosso banco, " + 
+        "sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.", 
+        numAgencia, numConta, saldoConta);
+
+        scanner.close();
+        
     }
 }
